da9d64e404dbba1bc1880f398071968e9b0bc810
kenzopanel <65157916+kenzopanel@users.noreply.github.com>
2022-02-03 23:10:10 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let path = window.location.pathname.split('/');
+
+if (path[1] == 'tugassiswa.php') {
+    $('section.content').append('<a href="#" class="btn btn-primary btn-lg" style="width:100%;margin-bottom:10px;">Cari Kunci Jawaban</a>');
+}
