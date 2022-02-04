a49e05bdfc05e87e463b94069a9a3523edddbd32
kenzopanel <65157916+kenzopanel@users.noreply.github.com>
2022-02-04 15:09:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var npath = window.location.pathname.split('/');
 
 if (npath[1] == 'tugassiswa.php') {
+    [...document.getElementsByClassName("mine")].forEach(e => e.remove());
+
     var div = document.createElement('div');
-    div.innerHTML = '<a href="http://smansawiok.epizy.com/" class="btn btn-primary btn-lg" style="width:100%;margin-bottom:10px;" target="_blank">Cari Kunci Jawaban</a>';
-    document.querySelector("section.content").appendChild(div);
+    div.innerHTML = '<a href="http://smansawiok.epizy.com/" class="btn btn-primary btn-lg mine" style="width:100%;margin-bottom:10px;" target="_blank">Cari Kunci Jawaban</a>';
+    document.querySelector("section.content").prepend(div);
 }
